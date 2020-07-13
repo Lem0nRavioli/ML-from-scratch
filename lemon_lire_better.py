da969c7a76043bfd4108eee1b47d0b79d4f2a849
Lem0nRavioli <57152284+Lem0nRavioli@users.noreply.github.com>
2020-07-13 17:11:27 +0200

    Add files via upload
    Adding a matrix version of linear regression + regularization
    Light update to the Logistic Regression file

--- lemon_lire_better.py
+++ lemon_lire_better.py
@@ -3,8 +3,9 @@ import numpy as np
 
 
 class LemonRegression:
-    def __init__(self, alpha=.1, max_iter=2000, threshold=10**-10, init_random=False, bias=.5):
+    def __init__(self, alpha=.1, max_iter=20000, threshold=10**-10, init_random=False, bias=.5,regularization=0):
         self.alpha = alpha
+        self.regul = regularization
         self.weights = None
         self.labels = None
         self.features = None
@@ -24,16 +25,14 @@ class LemonRegression:
         self.labels = np.array(labels)
         self.gradient_descent()
 
-    def predict(self, features, percent=False):
+    #update this
+    def predict(self, features):
         features = np.array(features)
         # add 1 for the intercept multiplier
         features = np.insert(features, 0, 1, axis=1)
-        predict_coef = np.dot(features,self.weights)
-        vfunc = np.vectorize(lambda x: 1 if x >= self.bias else 0)
-        predict = vfunc(predict_coef)
-        if percent: return predict_coef # np.insert(predict_coef,[1],predict,axis=1)
-        return predict
+        return np.dot(features,self.weights)
 
+    # update this
     def score(self, features, labels):
         predictions = self.predict(features).T[0]
         labels = np.array(labels)
@@ -63,6 +62,8 @@ class LemonRegression:
             current_score = self.cost_function(self.features,self.labels)
             if abs(previous_score-current_score) < self.threshold:
                 running = False
+                # uncomment this to observe the effect of regularization
+                #print(iter)
             previous_score = current_score
             # uncomment those to follow gradient descent
             #print("iter : " + str(iter) + " current cost:" + str(previous_score))
@@ -79,58 +80,17 @@ class LemonRegression:
 
     def cost_function(self, features, labels):
         hypo = np.dot(self.weights.T, features.T) # θTx (more like θTxT)
-        vfunc = np.vectorize(lambda x: 1/(1+np.exp(-x)))
-        sig = vfunc(hypo) # 1/(1+e^-θTx) (θTxT because of the format)
-        total_cost = [-y * np.log(x) - (1 - y) * np.log(1 - x) for y,x in zip(labels, sig[0])]
-        return sum(total_cost) / len(features)
+        cost = (hypo[0] - labels)**2
+        regularization = self.regul*(self.weights.T[0]**2)
+        return (sum(cost) + sum(regularization)) / len(features)
 
 
-
-# don't mind that, just testing code
-######################################################################################################################
-
-'''a = np.array([[1,2,3],[3,3,2],[1,4,1],[3,3,1]], dtype=float)
-b = np.ones((4,3))
-c = np.random.randint(-5,5,(4,3))
-theta = np.array([[1],[3],[4]])
-htheta = np.dot(a, theta)
-labels = np.array([[0,1,1,0]])
-delta = 1/len(a) * (np.sum(0))'''
-'''print(htheta)
-print((htheta - labels.T) * a.T[1])
-print(len(a))
-print(np.e**2) # this one goes further
-print(np.exp(2))
-a = np.array(a)
-print(a)'''
-
-'''model = LemonRegression()
-model.fit(a, labels[0])'''
-'''print(model.labels)
-print(np.dot(model.weights.T, model.features.T))
-print(np.log(1))
-
-hypo = np.dot(model.weights.T, model.features.T)
-vfunc = np.vectorize(lambda x: 1/(1+np.exp(-x)))
-sig = vfunc(hypo)
-print(sig[0])
-print(model.features)
-print(model.labels)
-print(model.score(model.features, model.labels))
-print(np.log(.5))
-
-f = np.ones((1,4))
-g = np.ones(4)
-g /= 2
-print(f,g)
-print((f - g).T)'''
-
-
-'''print(model.weights)
-print(model.predict(a))
-print(model.score(a, labels[0]))'''
-'''h = np.ones((4,1))
-i = np.ones((4,1))
-j = np.insert(h,[1],i,axis=1)
-print(i)
-print(j)'''
+a = np.array([1,2,3])
+a = a**2
+print(a)
+features = np.array([[1,2],[2,2],[4,1],[3,2]])
+labels = np.array([5,8,13,11])
+model = LemonRegression(regularization=10)
+model.fit(features,labels)
+print(model.weights_())
+print(model.predict([[2,4],[3,3]]))
